17ed6032cad17b94824ceca263efdf175ba4961f
itznav1216 <ayushvardhan820@gmail.com>
2026-02-10 15:11:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// CurricuForge – Safe, minimal JS
-// No loops, no DOM duplication, no UI bugs
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("CurricuForge UI loaded successfully");
-});
